cc26460d537e106250f60eeadfd09635e981e689
Muhammed Ibrahim <aminmuhammad18@gmail.com>
2021-10-08 23:03:22 +0100

    Added stripe server

--- client/src/components/stripe-button/stripe-button.component.jsx
+++ client/src/components/stripe-button/stripe-button.component.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import StripeCheckout from 'react-stripe-checkout';
+import axios from 'axios';
 
 const StripeButton = ({ price }) => {
   const priceForStripe = price * 100;
@@ -7,8 +8,23 @@ const StripeButton = ({ price }) => {
     'pk_test_51JaQOEJ5PwAOxOdzMveo8Ssr1p9dsqKx9JmEBGu7KhRfi7oDC36uGhvkG98E20CeagaTpkha5QD55bFqvFF8YWl300sa4NmWEx';
 
   const onToken = (token) => {
-    console.log(token);
-    alert('Payment Successful');
+    // console.log(token);
+    // alert('Payment Successful');
+    axios({
+      url: 'payment',
+      method: 'post',
+      data: {
+        amount: priceForStripe,
+        token,
+      },
+    })
+      .then((response) => {
+        alert('Payment successful');
+      })
+      .catch((error) => {
+        console.log('Payment error: ', JSON.parse(error));
+        alert('There was an issue');
+      });
   };
 
   return (
